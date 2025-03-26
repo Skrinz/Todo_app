18f2e1118ef004de85739074ed273bfae1bd3138
StarMysteries <khalelvega@gmail.com>
2025-03-26 19:58:58 +0800

    Disabled landscape mode in Android

--- todo_client/todo_client/Platforms/Android/MainActivity.cs
+++ todo_client/todo_client/Platforms/Android/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Views;
 namespace todo_client;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
+    ScreenOrientation = ScreenOrientation.Portrait,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
                            ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
