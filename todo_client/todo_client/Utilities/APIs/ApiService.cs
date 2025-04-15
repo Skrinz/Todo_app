9c02bb6ad6cc02c3e179d81cfb5ad445c8707b50
Skrinz <111281257+Skrinz@users.noreply.github.com>
2025-04-15 08:12:41 +0800

    Optimized the error display format

--- todo_client/todo_client/Utilities/APIs/ApiService.cs
+++ todo_client/todo_client/Utilities/APIs/ApiService.cs
@@ -59,9 +59,11 @@ namespace todo_client
                 var json = JsonSerializer.Serialize(requestData);
                 var content = new StringContent(json, Encoding.UTF8, "application/json");
                 
+                //constructing the API URl register endpoint
                 var url = $"{Constants.SERVERURL}{Constants.REGISTER}";
                 Console.WriteLine($"Attempting to connect to: {url}");
                 
+                //Requesting register to the server
                 var response = await _httpClient.PostAsync(url, content);
                 Console.WriteLine($"Response status code: {response.StatusCode}");
 
@@ -71,8 +73,26 @@ namespace todo_client
                 }
                 else
                 {
-                    var error = await response.Content.ReadAsStringAsync();
-                    return (false, $"Registration failed: {error}");
+                    var errorContent = await response.Content.ReadAsStringAsync();
+                    try
+                    {
+                        using var jsonDoc = JsonDocument.Parse(errorContent);
+                        if (jsonDoc.RootElement.TryGetProperty("errors", out var errorProperty))
+                        {
+                            return (false, errorProperty.GetString() ?? "Unknown error");
+                        }
+                        else if (jsonDoc.RootElement.TryGetProperty("message", out var messageProperty))
+                        {
+                            return (false, messageProperty.GetString() ?? "Unknown error");
+                        }else
+                        {
+                            return (false, "Unexpected error response");
+                        }
+                    }
+                    catch
+                    {
+                        return (false, "Error with your inputs");
+                    }
                 }
             }
             catch (Exception ex)
@@ -96,10 +116,12 @@ namespace todo_client
         
                 var json = JsonSerializer.Serialize(requestData);
                 var content = new StringContent(json, Encoding.UTF8, "application/json");
-        
+                
+                //Constructing API URL login endpoint
                 var url = $"{Constants.SERVERURL}{Constants.LOGIN}";
                 Console.WriteLine($"Attempting to connect to: {url}");
         
+                //Requesting login to the server
                 var response = await _httpClient.PostAsync(url, content);
                 Console.WriteLine($"Response status code: {response.StatusCode}");
         
@@ -109,8 +131,27 @@ namespace todo_client
                 }
                 else
                 {
-                    var error = await response.Content.ReadAsStringAsync();
-                    return (false, $"Login failed: {error}");
+                    var errorContent = await response.Content.ReadAsStringAsync();
+                    try
+                    {
+                        using var jsonDoc = JsonDocument.Parse(errorContent);
+                        if (jsonDoc.RootElement.TryGetProperty("errors", out var errorProperty))
+                        {
+                            return (false, errorProperty.GetString() ?? "Unknown error");
+                        }
+                        else if (jsonDoc.RootElement.TryGetProperty("message", out var messageProperty))
+                        {
+                            return (false, messageProperty.GetString() ?? "Unknown error");
+                        }
+                        else
+                        {
+                            return (false, "Unexpected error response");
+                        }
+                    }
+                    catch
+                    {
+                        return (false, "Error with your inputs");
+                    }
                 }
             }
             catch (Exception ex)
--- todo_client/todo_client/obj/Debug/net9.0-android/todo_client.AssemblyInfo.cs
+++ todo_client/todo_client/obj/Debug/net9.0-android/todo_client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todo_client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9ff797011f25a1cb0fe54869139f657b86b2ebdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+82930efd451c1d4b34641885728c158fea373f64")]
 [assembly: System.Reflection.AssemblyProductAttribute("todo_client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todo_client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
